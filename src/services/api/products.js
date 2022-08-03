3f015f39453a6f52896250ad7f6f08a394a11449
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-08-03 15:39:25 -0500

    [UPDATE] Next config

---
+++ src/services/api/products.js
@@ -0,0 +1,30 @@
+import endPoints from '@services/api';
+import axios from 'axios';
+
+const addProduct = async (body) => {
+  const config = {
+    headers: {
+      accept: '*/*',
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await axios.post(endPoints.products.addProducts, body, config);
+  return response.data;
+};
+
+const deleteProduct = async (id) => {
+  const response = await axios.delete(endPoints.products.deleteProducts(id));
+  return response.data;
+};
+const updateProduct = async (id, body) => {
+  const config = {
+    headers: {
+      accept: '*/*',
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await axios.put(endPoints.products.updateProducts(id), body, config);
+  return response.data;
+};
+
+export { addProduct, deleteProduct, updateProduct };
